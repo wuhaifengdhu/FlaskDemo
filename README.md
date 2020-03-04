416511a492a86ccaec06a39e9a9ff0ba209ef4a3
Devin <haifwu@paypal.com>
2020-03-04 22:12:28 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# FlaskDemo
+Start a schedule service in backend with the Flask app. 
+
+Result:
+![run every 30 seconds](https://raw.githubusercontent.com/wuhaifengdhu/FlaskDemo/master/doc/every-30-seconds.png)
--- task.py
+++ task.py
@@ -11,13 +11,14 @@ class Task(object):
         print 'Task init and start schedule'
 
     def do_task(self, para):
-        print 'Now I am doing my job (%s) with para %s' % (self.__name, para)
+        pit = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print 'Now I am doing my job (%s) with para (%s) at %s' % (self.__name, para, pit)
 
     def start_schedule_task(self):
-        para = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        schedule.every(30).seconds.do(self.do_task, para)
+        schedule.every(30).seconds.do(self.do_task, "whatever parameters")
         while True:
-            schedule.run_all(5)
+            schedule.run_pending()
+            time.sleep(1)
 
     def start_backend_schedule(self):
         threading.Thread(target=self.start_schedule_task).start()
